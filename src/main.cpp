4b477b5d1b0ad5f0215cb2d0976ba02706c2a401
Waasila Asif <wasif.bscs24seecs@seecs.edu.pk>
2026-07-10 19:20:24 +0500

    Ok I am actually proud of what I have built right now.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #if defined(_WIN32)
 #include <io.h>
 #include <process.h>
+#include<direct.h>
 #else
 #include <sys/types.h> // Defines pid_t
 #include <unistd.h>    // Defines fork() and execvp()
@@ -15,6 +16,22 @@
 
 using namespace std;
 void lsh_loop();
+int lsh_cd (vector <string>& args);
+int lsh_help (vector <string>& args);
+int lsh_exit (vector <string>& args);
+vector<string> builtin_str = {
+    "cd",
+    "help",
+    "exit"
+};
+int (*builtin_func[])(vector<string> &args) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit
+};
+int lsh_num_builtins(){
+    return sizeof(builtin_str);
+}
 string lsh_read_line();
 vector <string> lsh_split_line(string line);
 bool lsh_execute(vector<string> args);
@@ -40,6 +57,7 @@ void lsh_loop(){
 string lsh_read_line(){
     string line = "";
     getline(cin, line);
+    if (!getline (cin , line)){
     if(cin.eof()){
         exit (EXIT_SUCCESS);
 
@@ -47,13 +65,13 @@ string lsh_read_line(){
     else {
         perror("lsh: read line error");
         exit (EXIT_FAILURE);
-    }
+    }}
     return line;
 }
 vector <string> lsh_split_line(string line) {
     vector<string> tokens ;
     string current_token;
-    while(1){
+  
         if(line.empty()){
             cout<<"> lsh: allocation error. No command entered"<<endl<<">"
             ;
@@ -80,7 +98,7 @@ vector <string> lsh_split_line(string line) {
             }
 
         
-    }
+    
     return tokens;
 }
 
@@ -99,4 +117,37 @@ int lsh_launch(vector<string> args){
 #else
    
 #endif
+}
+int lsh_cd(vector<string> args){
+    if (args[1].data() == NULL) {
+        cout<<"lsh: expected an argument to the cd command, Need an argument to move to the directory required"<<endl;
+
+    } else{
+        if (_chdir((args[1]).c_str()) != 0){
+            perror("lsh");
+
+        }
+
+    }
+    return 1;
+}
+
+int lsh_help(vector<string> args){
+    int i;
+    cout<<"My implementation of LSH inspired by Stepher Brennan's implementation"<<endl;
+    cout<<"Most of the stuff online was in C and was only for Unix like systems. As like most people I starred coding from windows I wanted to make a cross OS shell"<<endl;
+    cout<<"So here is my humble attempt"<<endl;
+    cout<<"Type program names and arguments, and hit enter."<<endl;
+    cout<<"The following are built in:"<<endl;
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    cout<<i<<".  "<<builtin_str[i]<<endl;
+  }
+
+  return 1;
+   
+}
+int lsh_exit(vector<string> args)
+{
+  return 0;
 }
